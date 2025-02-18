751cafae87395e3957174cdce4d3d8385be79625
Rafael <rafaroga46@gmail.com>
2025-02-17 23:05:24 -0300

    first commit

---
+++ src/main/java/com/gmail/rafaroga46/CamilMovieWatch/CamilMovieWatchApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.rafaroga46.CamilMovieWatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamilMovieWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamilMovieWatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmail/rafaroga46/CamilMovieWatch/CamilMovieWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.rafaroga46.CamilMovieWatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamilMovieWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
